50bd4b06716d6eeb80ce8ad8901d4edd015c3efc
OlegN63 <129432125+OlegN63@users.noreply.github.com>
2023-03-30 22:32:09 +0300

    Add files via upload

---
+++ CZ.py
@@ -0,0 +1,116 @@
+''' Игра "Крестики - нолики" '''
+
+#Взял с вебинара greet и проверка на числа
+field = globals()
+field = [[" "] * 3 for i in range(3)]
+
+def greet():
+    print("-------------------")
+    print("  Приветсвуем вас  ")
+    print("      в игре       ")
+    print("  крестики-нолики  ")
+    print("-------------------")
+    print(" формат ввода: x y ")
+    print(" x - номер строки  ")
+    print(" y - номер столбца ")
+    print("-------------------")
+    print()
+
+
+
+def request():
+    while True:
+        cords = input("Введите координаты: ").split()
+
+        if len(cords) != 2:
+            print(" Введите 2 координаты! ")
+            continue
+
+        x, y = cords
+
+        if not (x.isdigit()) or not (y.isdigit()):
+            print(" Введите числа! ")
+            continue
+
+        x, y = int(x), int(y)
+
+        if 0 > x or x > 2 or 0 > y or y > 2:
+            print(" Координаты вне диапазона! ")
+            continue
+
+        if field[x][y] != " ":
+            print(" Клетка занята! ")
+            continue
+
+        return x, y
+
+
+
+
+
+def show():
+    print(f"   0  1  2")
+    for i in range(3):
+        print(f" {i} {field[i][0]} |{field[i][1]} |{field[i][2]}")
+        print('-----------')
+
+def check_win():
+    win_cord = (((0, 0), (0, 1), (0, 2)), ((1, 0), (1, 1), (1, 2)), ((2, 0), (2, 1), (2, 2)),
+                ((0, 2), (1, 1), (2, 0)), ((0, 0), (1, 1), (2, 2)), ((0, 0), (1, 0), (2, 0)),
+                ((0, 1), (1, 1), (2, 1)), ((0, 2), (1, 2), (2, 2)))
+    for cord in win_cord:
+        symbols = []
+        for c in cord:
+            symbols.append(field[c[0]][c[1]])
+        if symbols == ["X", "X", "X"]:
+            print("Выиграл X!!!")
+            return True
+        if symbols == ["0", "0", "0"]:
+            print("Выиграл 0!!!")
+            return True
+    return False
+
+
+
+greet()
+field = [[" "] * 3 for i in range(3) ]
+
+def crosses_zeroes():
+    greet()
+
+    show()
+
+    request()
+
+
+count = 0
+while True:
+    count += 1
+    show()
+    if count % 2 == 1:
+        print(" Ходит крестик!")
+    else:
+        print(" Ходит нолик!")
+
+    x, y = request()
+
+    if count % 2 == 1:
+        field[x][y] = "X"
+    else:
+        field[x][y] = "0"
+
+    if check_win():
+        break
+
+    if count == 9:
+        print(" Ничья!")
+        break
+
+
+
+
+
+
+
+
+crosses_zeroes()
